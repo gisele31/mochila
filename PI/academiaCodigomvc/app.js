e4cb81430826d95abfbae5847cb6f01e389bf20f
Gisele31 <gisele_tomazini@hotmail.com>
2022-03-18 18:05:44 -0300

    1 correção-guilherme

--- PI/academiaCodigomvc/app.js
+++ PI/academiaCodigomvc/app.js
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/pagamentos', pagamentosRouter);
 app.use('/planos', planosRouter);
+app.use('/pagamentos', pagamentosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
